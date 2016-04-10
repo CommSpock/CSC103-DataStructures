f2db3b7510b75e26f8b92a0101ff155f60fecd8a
CommSpock <CommSpock@users.noreply.github.com>
2016-04-10 16:56:18 -0400

    Runway Final Draft
    Testing and documentation complete.

--- Runway.java
+++ Runway.java
@@ -4,39 +4,30 @@
 // Authors: Kevin Soule, Rafael Ferrer, and Carmen Chiu
 // Due Date: Monday 4/11/16
 
-/*****************************************************************************************************************  
-* An UnboundedInt is a positive whole number, base 10 integer, of arbitrary length. The UnboundedInt class allows 
-* for the creation of unbounded integers and provides methods for performing basic arithmetic operations with
-* UnboundedInt's (addition and multiplication).
+/*************************************************************************************************************************
+* The Runway class is used in conjunction with the Plane class and the RunwaySimulation class to simulate planes taking off 
+* and landing on a runway. Every Runway is assigned a current flight operation, as well as the times required for the
+* Runway to process a landing or take-off.
 *
 * @note
-* 	(1) An UnboundedInt can be increased indefinitely after it's created, but the maximum size is limited 
-*   by the amount of free memory on the machine. The UnboundedInt(int... elements), addFront(int element), 
-*   addEnd(int element), add(UnboundedInt addendInt), multiply(UnboundedInt multiplierInt), and clone() 
-*   methods will result in an OutOfMemoryError when free memory is exhausted.
+*   (1) A Runway can either be set to idle, or the take-off or landing flight operations.
 *   <p>
-*   (2) The elements of an UnboundedInt are stored in a sequence in increasing order from the element containing 
-*   the ones place to the element containing the highest multiple of ten.
+*   (2) The runway usually receives its current flight operation from the Plane class, otherwise it is idle.
 *   <p>
-*   (3) Each element of an UnboundedInt contains a positive one, two, or three digit integer that represents
-*   three placeholders of UnboundedInt.
+*   (3) The simulation time a runway takes to process a flight operation depends on the time assigned to that operation.
 *
 * @version
-*   March 20, 2016
-*****************************************************************************************************************/
+*   April 10, 2016
+*************************************************************************************************************************/
 
 class Runway
 {
 	// Invariant of the Runway class:
-		//   1. The number of three integer elements in the UnboundedInt is in the instance variable nodeCount.
-		//   2. The hundreds place in the UnboundedInt is in the instance variable head, which is also the first 
-		//      element of the linked list holding the elements of the UnboundedInt.
-		//   3. The highest power of ten in the UnboundedInt is in the instance variable tail, which is also the
-		//      last element of the linked list holding the elements of the UnboundedInt.
-		//   4. The instance variable current points to the currently selected element of the linked list. If there 
-		//      is no currently selected element, the cursor will be null and at the end of the linked list.
-		//   5. We care what is stored in every element that the head links to (directly or indirectly). We do not 
-		//      care about any elements that the head has no link to (directly or indirectly).
+	//   1. The timeForLanding variable is the number of minutes it takes for a Plane to land.
+	//   2. The timeForTakeoff variable is the number of minutes it takes for a plane to take-off.
+	//   3. The runwayTimeLeft variable is the number of minutes left in the current flight operation the runway is processing.
+	//   4. The operation variable is the character that signifies the current flight operation the runway is processing.
+	//      The available flight operations are 'I' for idle, 'L' for landing, or 'T' for take-off.
 	
 	
 	/// Private Instance Variables ///
@@ -44,26 +35,32 @@ class Runway
 	private int timeForLanding;
 	private int timeForTakeoff;
 	private int runwayTimeLeft;
-	private char operation; //operation can be: 'I' – Idle, 'L' - Landing, or 'T' - Takeoff
+	private char operation;
 	
 	
 	/// Constructor ///
+	
 	/**
-	 * Description
-	 * @param
-	 *   
+	 * A constructor to create a new Runway object with the times required for processing take-offs and landings.
+	 * @param time_takeoff
+	 *   The number of minutes it takes for a Plane to take-off.
+	 * @param time_landing
+	 *   The number of minutes it takes for a Plane to land.
 	 * @precondition
-	 *   
-	 * @postcondition / return
-	 *   
-	 * @exception
-	 *   
-	 * @note
-	 *   
+	 *   The arguments passed for time_takeoff and time_landing must be integers greater than zero.
+	 * @postcondition
+	 *   A new Runway object has been created and assigned the times required for processing take-offs and landings.
+	 * @exception IllegalArgumentException
+	 *   Will occur if the time_takeoff or time_landing arguments passed are not integers greater than zero.
 	 **/
 	public Runway(int time_takeoff, int time_landing)
 	{
-		//set the time for landing, time for takeoff, and the operation to idle.
+		//Verify that the arguments passed are positive integers
+		if (time_takeoff < 1 || time_landing < 1){
+			throw new IllegalArgumentException("Runway's arguments must be integers greater than zero!");
+		}
+		
+		//Set the time for landing, time for take-off, and set the operation to idle.
 		timeForLanding = time_landing;
 		timeForTakeoff = time_takeoff;
 		operation = 'I';
@@ -74,17 +71,10 @@ class Runway
 	/// Accessor Methods ///
 	
 	/**
-	 * Description
-	 * @param
-	 *   
-	 * @precondition
-	 *   
-	 * @postcondition / return
-	 *   
-	 * @exception
-	 *   
-	 * @note
-	 *   
+	 * An accessor method that tells whether the runway is idle or in use for a flight operation.
+	 * @param none  
+	 * @return
+	 *   A true or false boolean value pertaining to whether the runway is idle or currently in use for a flight operation.
 	 **/
 	public boolean isBusy()
 	{
@@ -93,84 +83,72 @@ class Runway
 	}//End isBusy() Method
 	
 	/**
-	 * Description
-	 * @param
-	 *   
-	 * @precondition
-	 *   
-	 * @postcondition / return
-	 *   
-	 * @exception
-	 *   
+	 * An accessor method that returns the time left in the current flight operation's use of the runway.
+	 * @param none 
+	 * @return
+	 *  Returns an integer value representing the number of minutes left in the use of the runway for a flight operation.
 	 * @note
-	 *   
+	 *   The time left is decremented by the reduceRemainingTime() method.
 	 **/
 	public int getTimeLeft()
 	{
 		return runwayTimeLeft;
-	}
+		
+	}//End getTimeLeft() Method
 	
 	/**
-	 * Description
-	 * @param
-	 *   
-	 * @precondition
-	 *   
-	 * @postcondition / return
-	 *   
-	 * @exception
-	 *   
-	 * @note
-	 *   
+	 * An accessor method that returns a character representing the flight operation the Runway is processing.
+	 * @param none
+	 * @return
+	 *   Returns a character that represents the flight operation the Runway is currently being used for.
+	 *   Returns 'T' for take-off, 'L' for landing or 'I' for idle.
 	 **/
 	public char kindOfOperation()
 	{
-		//returns the type of operation the runway is currently being used for. 
 		return operation;
 		
 	}//End kindOfOperation() Method
 	
 	/**
-	 * Description
-	 * this states what operation the runway is currently doing
-	 * @param
-	 *   
-	 * @precondition
-	 *   
-	 * @postcondition / return
-	 *   
-	 * @exception
-	 *   
+	 * An accessor method that returns the flight operation the Runway is currently processing as a String value (as opposed to a single character).
+	 * @param none
+	 * @return
+	 *   Returns a String statement that states what flight operation the runway is currently being used for.
 	 * @note
-	 *   
+	 *   Will return "taking-off" if the plane is taking off, "landing" if the plane is landing, or "Idle" if the runway is not in use.
+	 *   "Idle" is the Runway's default operation.
 	 **/
 	public String getOperationStatement()
 	{
 		String statement = "Idle";
+		
 		if (kindOfOperation()=='T'){
-			statement = "taking off";
+			statement = "taking-off";
 		}
 		else if (kindOfOperation()=='L'){
 			statement = "landing";
 		}
+		
 		return statement;
+		
 	}//End getOperationStatement() Method
 	
 	
-	/// Setter Methods ///
+	/// Modifier Methods ///
 	
 	/**
-	 * Description
-	 * @param
-	 *   
+	 * A modifier method that assigns the Runway a flight operation.
+	 * @param typeOfUse
+	 *   A character that tells the Runway what flight operation it should start processing.
+	 *   Enter 'T' for take-off, 'L' for landing, or 'I' for idle.
 	 * @precondition
-	 *   
-	 * @postcondition / return
-	 *   
-	 * @exception
-	 *   
+	 *   The argument passed for typeOfUse must be either 'L' for landing, 'T' for take-off, or 'I' for idle (in which case the Runway is not being used).
+	 * @postcondition
+	 *   The Runway is now set to the flight operation assigned it and can begin processing the simulation time required to complete the flight operation.
+	 * @exception IllegalArgumentException
+	 *   Will occur if the typeOfUse argument passed is not 'T', 'L', or 'I'.
 	 * @note
-	 *   
+	 *   Simulation time for processing flight operations varies depending on the operation.
 	 **/
 	public void startUsingRunway(char typeOfUse)
 	{
@@ -179,7 +157,7 @@ class Runway
 			operation = 'L';
 			runwayTimeLeft = timeForLanding;
 		}
-		//if typeOfUse is 'T' - then the operation is take off and set the runway time left to the time it takes for takeoff.
+		//if typeOfUse is 'T' - then the operation is take off and set the runway time left to the time it takes for take-off.
 		else if (typeOfUse == 'T'){
 			operation = 'T';
 			runwayTimeLeft = timeForTakeoff;
@@ -189,21 +167,17 @@ class Runway
 			operation = 'I';
 			runwayTimeLeft = 0;
 		}
+		else {
+			throw new IllegalArgumentException("Enter 'T' for take-off, 'L' for landing, or 'I' for idle. No other arguments will be accepted!");
+		}
 		
 	}//End startUsingRunway(char typeOfUse) Method
 	
 	/**
-	 * Description
-	 * @param
-	 *   
-	 * @precondition
-	 *   
-	 * @postcondition / return
-	 *   
-	 * @exception
-	 *   
-	 * @note
-	 *   
+	 * A modifier method that decrements the amount of time remaining to complete the current flight operation on the runway by one minute.
+	 * @param none
+	 * @postcondition
+	 *   The simulation time left to complete the current flight operation has been decremented by one minute. 
 	 **/
 	public void reduceRemainingTime()
 	{
