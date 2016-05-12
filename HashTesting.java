496aa8f67ed5affab5be27090da7c936ffacfc05
CommSpock <CommSpock@users.noreply.github.com>
2016-05-12 10:55:14 -0400

    HashTesting Class

---
+++ HashTesting.java
@@ -0,0 +1,26 @@
+// File: HashTesting.java
+
+// Project #4: Analysis of Hashing versus Double Hashing
+// Authors: Rafael Ferrer and Carmen Chiu
+// Due Date: Monday 5/22/16
+
+/****************************************************************************************************
+* A Table is an open-address hash table with a fixed capacity. The purpose is to show students how 
+* an open-address hash table is implemented. Programs should generally use java.util.Hashtable
+* rather than this hash table.
+*
+* @version
+*   May 12, 2016
+****************************************************************************************************/
+
+public class HashTesting
+{
+	public static void main(String[] args)
+	{
+		
+		
+	}//End Main Method
+	
+	
+	
+}//End HashTesting Class
